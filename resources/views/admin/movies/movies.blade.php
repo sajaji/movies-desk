3205419c9034f197159580545c253d365f427ad6
Sajaji Sajith <mohamedsajaji@icloud.com>
2024-02-20 12:04:28 +0530

    backend bug fixed

--- resources/views/admin/movies/movies.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="container">
-        <h1>Movies</h1>
-        <!-- Add your movie listing logic and HTML here -->
-    </div>
-@endsection
